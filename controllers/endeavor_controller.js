647ff0857de314f2a7c35b611ed312ec076d0ec0
adam fader <adamfader@gmail.com>
2016-08-14 00:19:20 -0400

    added new job db insert

--- controllers/endeavor_controller.js
+++ controllers/endeavor_controller.js
@@ -89,7 +89,7 @@ router.get('/endeavors', function (req, res) {
 // passes the values from the index.handlebars form and passes the db column name
 // redirects to .get /endeavors and reloads page
 router.post('/job/create', function (req, res) {
-  endeavor.create(['jobs'], ['jobtitle', 'jobdescription', 'posterid'], [req.body.job_title, req.body.job_description, req.body.poster_id], function () {
+  endeavor.create(['jobs'], ['title', 'description', 'userID', 'image', 'category', 'subcategory', 'bidding', 'jobstart', 'deadline', 'firmness', 'budget'], [req.body.job_title, req.body.job_description, req.body.user_id, req.body.image1, req.body.category, req.body.subcategory, req.body.bidding, req.body.start, req.body.end, req.body.firm, req.body.budget], function () {
     res.redirect('/endeavors');
   });
 });
