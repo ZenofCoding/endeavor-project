b59c56c200760731c095afb165580216c4d70de7
pkondeti <padma.kondeti@gmail.com>
2016-08-15 08:21:19 -0400

    updated connection file with adam's jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,40 @@
-/*
-Here is where you make the connection to the database and export and used by the O.R.M.
-*/
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'endeavor_db'
-  // password: 'sector001',
-  // database: 'my_schema'
-});
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
 
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
+var mysql = require('mysql');
+
+// we placed the connections in this source object
+var source = {
+    // localhost
+    localhost: {
+        port: 3306,
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'kopdakel41c0b6bm',
+        password: "c377x1xc6713qabw",
+        database: "pwdopoqcppufqavw"
+    },
+
+    // jawsDB
+    jawsDB: {
+        port: 3306,
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'kopdakel41c0b6bm',
+        password: "c377x1xc6713qabw",
+        database: "pwdopoqcppufqavw" 
+    }
+}
+
+// we use source.[name of connection] to hook into mysql
+var connection = mysql.createConnection(source.jawsDB || source.localhost);
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
