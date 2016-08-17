29922db781777bc47c2eced9e4ca42b603b5941c
pkondeti <padma.kondeti@gmail.com>
2016-08-17 03:07:22 -0400

    updated jobsearch

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,14 @@ var source = {
     // localhost
     localhost: {
         port: 3306,
-        host: 'localhost',
-        user: '',
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'kopdakel41c0b6bm',
         password: "c377x1xc6713qabw",
         database: "pwdopoqcppufqavw" 
+        // host: 'localhost',
+        // user: 'root',
+        // password: "",
+        // database: "my_schema" 
     },
 
 
@@ -27,7 +31,8 @@ var source = {
 }
 
 // we use source.[name of connection] to hook into mysql
-var connection = mysql.createConnection(source.jawsDB || source.localhost);
+ var connection = mysql.createConnection(source.jawsDB || source.localhost);
+//var connection = mysql.createConnection(source.localhost);
 
 
 connection.connect(function(err) {
