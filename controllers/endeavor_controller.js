7225206583c064b126db40d0ed40c5a03ad2b408
adam fader <adamfader@gmail.com>
2016-08-28 07:35:55 -0400

    added reply capability to messaging

--- controllers/endeavor_controller.js
+++ controllers/endeavor_controller.js
@@ -664,8 +664,36 @@ router.post('/send/new/message', isLoggedIn2, function(req, res) {
   });
 });
 
+// we will want this protected so you have to be logged in to use
+// we will use route middleware to verify this (the isLoggedIn2 function)
+router.post('/send/reply/message', isLoggedIn2, function(req, res) {
+  var condition = 'id = ' + req.body.parent;
+  endeavor.create(['pm'], ['receiver', 'sender', 'subject', 'message', 'parent'], [req.body.pm_receiver, req.body.pm_sender, req.body.pm_subject, req.body.pm_message, req.body.parent], function () {
+    if(req.body.pm_sender == req.body.orig_sender){
+      var hasreplies = '1';
+      var rread = '0';
+      var sread = '1';
+    }else{
+      var hasreplies = '1';
+      var rread = '1';
+      var sread = '0';   
+    }
+    endeavor.updateString(['pm'], { hasreplies: hasreplies, rread: rread, sread: sread}, condition, function () {
+    res.redirect('back');
+    });
+  });
+});
+
+// we will want this protected so you have to be logged in to use
+// we will use route middleware to verify this (the isLoggedIn2 function)
+// router.put('/update/reply/message', function(req, res) {
+//   var condition = 'id = ' + req.body.osid;
+//   endeavor.updateString(['pm'], { hasreplies: req.body.hasreplies, rread: req.body.rread, sread: req.body.sread}, condition, function () {
+//     res.send(inserted);
+//   });
+// });
+
 // sends all of the user's notifications
-// employeeID should work for all messages because of the way they are inserted
 router.get('/private/messages/:id', isLoggedIn, function(req, res) {
   var condition = 'employeeID = ' + req.params.id; 
   endeavor.allParentPm('pm', condition, function (pms) {
